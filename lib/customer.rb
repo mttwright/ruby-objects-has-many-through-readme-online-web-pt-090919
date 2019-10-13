f0ee78fd1618c103ba3fac11219358e01a839739
virtual-frame-7789 <mttwright@gmail.com>
2019-10-13 18:09:32 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,18 @@
 class Customer
   
+  @@all = []
+  
+  attr_accessor :name, :age
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all.push(self)
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
 end
